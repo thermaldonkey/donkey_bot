a754ba87b08433e5a52ef2db26adf4b07ba719f4
Charles Zink <charleszink@wswoff1.chuckscoin.local>
2016-03-10 12:20:26 -0800

    Adds title function

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
 import re
 import sys
+import json
+import requests
 
 from my_socket import TwitchChatSocket
 from initialize import join_room
 from read import get_user, get_message, tokenize_new_data
 from settings import CHANNEL
+from settings import STREAM_PASS
 
 def check_obj(obj, count):
     if obj:
@@ -88,6 +91,13 @@ class DonkeyBot(object):
                             self.socket.send_private_message("That ain't a number brah")
                     else:
                         self.socket.send_private_message("Sorry for the confusion {} That's a command for only {}".format(user, CHANNEL))
+                elif re.match('^!title', message.lower()):
+                	if user.lower() == CHANNEL:
+                		tokens = message.split()
+                		r = requests.put('https://api.twitch.tv/kraken/channels/xcouchleaguex', data=json.dumps({'channel': {'status': message[7:], 'delay': 0 }}), headers={'Accept': 'application/vnd.twitchtv.v3+json', 'Authorization': 'OAuth '+STREAM_PASS, 'Content-Type': 'application/json'})
+                		self.socket.send_private_message("Ok, I've updated the stream title.");
+                	else:
+                		self.socket.send_private_message("Sorry for the confusion {} That's a command for only {}".format(user, CHANNEL))
 
 if __name__ == '__main__':
     bot = DonkeyBot()
--- settings.py
+++ settings.py
@@ -3,3 +3,6 @@ PORT = 6667
 PASS = '' # OAuth key
 IDENTITY = '' # Bot's nickname on Twitch
 CHANNEL = '' # Channel bot should connect to
+
+# FOR !title AND !game COMMANDS
+STREAM_PASS = ''; # OAuth key (For streamer.. not bot. Requires channel_editor scope)
