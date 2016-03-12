d1c01500f133719722f4794fa0afab4bb70fc20e
Brad <brad@britecore.com>
2016-03-11 23:05:26 -0600

    Commands to add and remove points
    Also adds ability to customize point alias and response messages.

--- run.py
+++ run.py
@@ -4,8 +4,9 @@ import sys
 from my_socket import TwitchChatSocket
 from initialize import join_room
 from read import get_user, get_message, tokenize_new_data
-from settings import CHANNEL
+from settings import CHANNEL, POINTS_ALIAS, POINTS_COUNT, ADDED_POINTS, REMOVED_POINTS
 from db import connect
+from models import Viewer
 
 def check_obj(obj, count):
     if obj:
@@ -27,6 +28,61 @@ class DonkeyBot(object):
         self.socket = TwitchChatSocket()
         join_room(self.socket)
         self.read_buffer = ''
+
+    def get_viewer(self, nickname):
+        '''
+        Returns the first Viewer with the given nickname.
+
+        @param nickname (str) - Nickname for the requested viewer
+        @return (Viewer,None)
+        '''
+        return self.session.query(Viewer).filter_by(nickname=nickname).first()
+
+    def add_viewer(self, nickname):
+        '''
+        Adds a new viewer with the given nickname.
+
+        @param nickname (str) - Nickname for the new viewer
+        @return (Viewer)
+        '''
+        viewer = Viewer(nickname=nickname)
+        self.session.add(viewer)
+        self.session.commit()
+        return viewer
+
+    def get_points(self, user):
+        '''
+        Returns the point count for a viewer with the given nickname.
+
+        @param user (str) - Nickname of the viewer whose points are requested
+        @return (int)
+        '''
+        viewer = self.get_viewer(user)
+        return getattr(viewer, 'points', 0)
+
+    def add_points(self, nickname, points):
+        '''
+        Adds given number of points to the viewer with the given nickname. Will add the viewer if no
+        matching one already exists.
+
+        @param nickname (str) - Nickname of the viewer
+        @param points (int) - Number of points to award
+        '''
+        viewer = self.get_viewer(nickname) or self.add_viewer(nickname)
+        viewer.points += int(points)
+        self.session.commit()
+
+    def remove_points(self, nickname, points):
+        '''
+        Removes given number of points from the viewer with the given nickname. Will add the viewer
+        if no matching one already exists.
+
+        @param nickname (str) - Nickname of the viewer
+        @param points (int) - Number of points to demerit
+        '''
+        viewer = self.get_viewer(nickname) or self.add_viewer(nickname)
+        viewer.points = max(viewer.points - int(points), 0)
+        self.session.commit()
 
     def the_thing_donkey_bot_does(self):
         temp, self.read_buffer = tokenize_new_data(self.socket, self.read_buffer)
@@ -89,6 +145,27 @@ class DonkeyBot(object):
                             self.socket.send_private_message("That ain't a number brah")
                     else:
                         self.socket.send_private_message("Sorry for the confusion {} That's a command for only {}".format(user, CHANNEL))
+                elif re.match('^!(points|{})'.format(POINTS_ALIAS), message.lower()):
+                    # Command is customizable by the POINTS_ALIAS setting. Reports calling viewer's total points
+                    points = self.get_points(user)
+                    self.socket.send_private_message(POINTS_COUNT.format(viewer=user, points=points, point_alias=POINTS_ALIAS))
+                elif re.match('^!add', message.lower()):
+                    # Adds points to the given viewer. Only callable by the broadcaster.
+                    if user.lower() == CHANNEL:
+                        _, points, viewer = message.split()
+                        self.add_points(viewer, points)
+                        self.socket.send_private_message(ADDED_POINTS.format(points=points, viewer=viewer, point_alias=POINTS_ALIAS))
+                    else:
+                        self.socket.send_private_message("{} suh dude? You know you can't do that!".format(user))
+                elif re.match('^!remove', message.lower()):
+                    # Removes points from the given viewer, but will never go below 0 total points.
+                    # Only callable by the broadcaster.
+                    if user.lower() == CHANNEL:
+                        _, points, viewer = message.split()
+                        self.remove_points(viewer, points)
+                        self.socket.send_private_message(REMOVED_POINTS.format(points=points, viewer=viewer, point_alias=POINTS_ALIAS))
+                    else:
+                        self.socket.send_private_message("{} suh dude? You know you can't do that!".format(user))
 
 if __name__ == '__main__':
     bot = DonkeyBot()
--- settings.py
+++ settings.py
@@ -5,3 +5,13 @@ IDENTITY = '' # Bot's nickname on Twitch
 CHANNEL = '' # Channel bot should connect to
 # See http://docs.sqlalchemy.org/en/latest/core/engines.html#database-urls on this:
 DB_URL = 'dialect+driver://username:password@host:port/database'
+POINTS_ALIAS = 'points' # Alias for channel points
+# Response string for retrieving points count. Interpolations are required but everything else is
+# fair game to change!
+POINTS_COUNT = "{viewer} has {points} {point_alias}"
+# Response string for adding points to a viewer. Interpolations are required but everything else
+# is fair game to change!
+ADDED_POINTS = "Gave {viewer} {points} more {point_alias}"
+# Response string for removing points from a viewer. Interpolations are required but everything else
+# is fair game to change!
+REMOVED_POINTS = "Took {points} {point_alias} away from {viewer}"
